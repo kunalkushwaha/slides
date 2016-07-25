12978fec6b3ce823ee502f0f1ebc1f5f0b4dbbbf
Kunal Kushwaha <kushwaha_kunal_v7@lab.ntt.co.jp>
2016-07-25 11:18:03 +0900

    Profiling enabled

--- examples/webserver/main.go
+++ examples/webserver/main.go
@@ -1,16 +1,62 @@
 package main
 
 import (
+	"errors"
 	"fmt"
+	"net"
 	"net/http"
+	_ "net/http/pprof"
 )
 
 func handler(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "<b>This is my <i>Awesome WebServer</i> V2.0!</b>")
+	fmt.Fprintf(w, "This is DemoApp v2 @ ")
+	ip, err := externalIP()
+	if err != nil {
+		ip = "Unknown"
+	}
+	fmt.Fprintf(w, " IP: %s\n", ip)
+
 }
 
 func main() {
 	http.HandleFunc("/", handler)
-	fmt.Println("Server is listening at : 8080")
-	http.ListenAndServe(":8080", nil)
+	fmt.Println("Server is listening at : 5000")
+	http.ListenAndServe(":5000", http.DefaultServeMux)
+}
+
+func externalIP() (string, error) {
+	ifaces, err := net.Interfaces()
+	if err != nil {
+		return "", err
+	}
+	for _, iface := range ifaces {
+		if iface.Flags&net.FlagUp == 0 {
+			continue // interface down
+		}
+		if iface.Flags&net.FlagLoopback != 0 {
+			continue // loopback interface
+		}
+		addrs, err := iface.Addrs()
+		if err != nil {
+			return "", err
+		}
+		for _, addr := range addrs {
+			var ip net.IP
+			switch v := addr.(type) {
+			case *net.IPNet:
+				ip = v.IP
+			case *net.IPAddr:
+				ip = v.IP
+			}
+			if ip == nil || ip.IsLoopback() {
+				continue
+			}
+			ip = ip.To4()
+			if ip == nil {
+				continue // not an ipv4 address
+			}
+			return ip.String(), nil
+		}
+	}
+	return "", errors.New("are you connected to the network?")
 }
